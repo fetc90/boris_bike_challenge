075515f1c616c73c38e6b59c8cab7cd9f0fcdd7f
Francesca Chater <francescachater@gmail.com>
2019-05-01 14:47:58 +0100

    Adding working method in bike class

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,8 @@
 require './lib/docking_station'
 
-describe DockingStation do
-
+describe 'Docking station' do
+  it 'should release bike' do
+    docking_station = DockingStation.new
+    expect(docking_station).to respond_to(:release_bike)
+  end
 end
